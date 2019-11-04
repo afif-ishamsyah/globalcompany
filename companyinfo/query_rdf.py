c3098d096f9bafbed65abdb771f4749c127366fc
minymanz <afifhan95@gmail.com>
2019-11-04 15:33:37 +0700

    Company Details : more handling for unique company name (amazon.com, apple inc), use website instead of name to get details from dbpedia

--- companyinfo/query_rdf.py
+++ companyinfo/query_rdf.py
@@ -9,14 +9,73 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 g = ConjunctiveGraph()
 
 
-def getCompanyDataOnline(param):
+# def getCompanyDataOnline(param):
+#     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+#     sparql.setQuery("""
+#     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+#     SELECT (lcase(str(?s)) as ?name) (str(?p) as ?pred)  (str(?o) as ?obj)
+#     WHERE { ?s a dbo:Company .
+#             ?s ?p ?o .
+#             FILTER ( lcase(str(?s)) = 'http://dbpedia.org/resource/""" + str(param) + """' )} """)
+            
+#     sparql.setReturnFormat(JSON)
+#     results = sparql.query().convert()
+#     return results
+
+def getCompanyDataOnline(param, web):
+    param  = str(param).replace(' ','_')
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     sparql.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT (lcase(str(?s)) as ?name) (str(?p) as ?pred)  (str(?o) as ?obj)
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    SELECT DISTINCT 
+           (str(?result_topic) as ?str_topic)
+           (str(?result_wikipageid) as ?str_wikipageid)
+           (str(?result_latitude) as ?str_latitude)
+           (str(?result_longitude) as ?str_longitude)
+           (str(?result_abstract) as ?str_abstract)
+           (str(?result_assets) as ?str_assets)
+           (str(?result_equity) as ?str_equity)
+           (str(?result_location) as ?str_location)
+           (str(?result_netincome) as ?str_netincome)
+           (str(?result_operatingincome) as ?str_operatingincome)
+           (str(?result_revenue) as ?str_revenue)
+           (str(?result_areaserved) as ?str_areaserved)
     WHERE { ?s a dbo:Company .
-            ?s ?p ?o .
-            FILTER ( lcase(str(?s)) = 'http://dbpedia.org/resource/""" + str(param) + """' )} """)
+            BIND('Not Exist' as ?default_string)
+            BIND( 0 as ?default_number)
+            OPTIONAL {?s <http://xmlns.com/foaf/0.1/isPrimaryTopicOf> ?topic .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/wikiPageID> ?wikipageid .}
+            OPTIONAL {?s <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?latitude .}
+            OPTIONAL {?s <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?longitude .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/abstract> ?abstract .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/assets> ?assets .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/equity> ?equity .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/location> ?location .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/netIncome> ?netincome .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/operatingIncome> ?operatingincome .}
+            OPTIONAL {?s <http://dbpedia.org/ontology/revenue> ?revenue .}
+            OPTIONAL {?s <http://dbpedia.org/property/areaServed> ?areaserved .}
+            OPTIONAL {?s foaf:homepage ?website .}
+            BIND(REPLACE(STR(?website), "https://www.", "", "i") AS ?homepage1)
+            BIND(REPLACE(STR(?homepage1), "http://www.", "", "i") AS ?homepage2)
+            BIND(REPLACE(STR(?homepage2), "https://", "", "i") AS ?homepage3)
+            BIND(REPLACE(STR(?homepage3), "http://", "", "i") AS ?homepage_clean)
+            BIND(COALESCE(?topic, ?default_string) as ?result_topic)
+            BIND(COALESCE(?wikipageid, ?default_string) as ?result_wikipageid)
+            BIND(COALESCE(?latitude, ?default_string) as ?result_latitude)
+            BIND(COALESCE(?longitude, ?default_string) as ?result_longitude)
+            BIND(COALESCE(?abstract, ?default_string) as ?result_abstract)
+            BIND(COALESCE(?assets, ?default_number) as ?result_assets)
+            BIND(COALESCE(?equity, ?default_number) as ?result_equity)
+            BIND(COALESCE(?location, ?default_string) as ?result_location)
+            BIND(COALESCE(?netincome, ?default_number) as ?result_netincome)
+            BIND(COALESCE(?operatingincome, ?default_number) as ?result_operatingincome)
+            BIND(COALESCE(?revenue, ?default_number) as ?result_revenue)
+            BIND(COALESCE(?areaserved, ?default_string) as ?result_areaserved)
+            FILTER (lang(?abstract) = 'en')
+            FILTER ( lcase(str(?s)) = 'http://dbpedia.org/resource/""" + param + """' || lcase(?homepage_clean) = '""" + web + """')} LIMIT 1""")
+
             
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -29,32 +88,40 @@ def getCompanyData(param):
     company_id = '<http://globalcompany.org/' + param + '>'
 
     stringquery = """SELECT (str(?name_label) as ?str_name_label) 
-                            (str(?country_label) as ?str_country_label)
-                            (str(?industry_label) as ?str_industry_label)
-                            (str(?year) as ?str_year)
-                            (str(?size) as ?str_size)
-                            (str(?locality_label) as ?str_locality_label)
-                            (str(?current) as ?str_current)
-                            (str(?total) as ?str_total)
-                            ?linkedinurl
-                            ?domainurl
-                    WHERE { ?sub <http://globalcompany.org/hasName> ?name .
-                            ?sub <http://globalcompany.org/hasOfficialWebsite> ?domain .
-                            ?sub <http://globalcompany.org/isIndustry> ?industry .
-                            ?sub <http://globalcompany.org/size_range> ?size .
-                            ?sub <http://globalcompany.org/locality> ?locality .
-                            ?sub <http://globalcompany.org/locatedInCountry> ?country .
-                            ?sub <http://globalcompany.org/hasLinkedinURL> ?linkedin .
-                            ?sub <http://globalcompany.org/current_employee> ?current .
-                            ?sub <http://globalcompany.org/total_employee_estimate> ?total .
+                            (str(?result_country) as ?str_country_label)
+                            (str(?result_industry) as ?str_industry_label)
+                            (str(?result_year) as ?str_year)
+                            (str(?result_size) as ?str_size)
+                            (str(?result_locality) as ?str_locality_label)
+                            (str(?result_current) as ?str_current)
+                            (str(?result_total) as ?str_total)
+                            (str(?result_linkedin) as ?linkedinurl)
+                            (str(?result_domain) as ?domainurl)
+                    WHERE { BIND('Not Exist' as ?default_string)
+                            """ + company_id + """ <http://globalcompany.org/hasName> ?name .
+                            OPTIONAL {""" + company_id + """ <http://globalcompany.org/hasOfficialWebsite> ?domain .}
+                            """ + company_id + """ <http://globalcompany.org/isIndustry> ?industry .
+                            OPTIONAL {""" + company_id + """ <http://globalcompany.org/size_range> ?size .}
+                            """ + company_id + """ <http://globalcompany.org/locality> ?locality .
+                            """ + company_id + """ <http://globalcompany.org/locatedInCountry> ?country .
+                            OPTIONAL {""" + company_id + """ <http://globalcompany.org/hasLinkedinURL> ?linkedin .}
+                            OPTIONAL {""" + company_id + """ <http://globalcompany.org/current_employee> ?current .}
+                            OPTIONAL {""" + company_id + """ <http://globalcompany.org/total_employee_estimate> ?total .}
+                            OPTIONAL {""" + company_id + """ <http://globalcompany.org/yearFound> ?year .  }
                             ?name <rdfs:label> ?name_label .
-                            ?country <rdfs:label> ?country_label .
-                            ?industry <rdfs:label> ?industry_label .
-                            ?locality <rdfs:label> ?locality_label .
-                            BIND(CONCAT("https://", STR( ?linkedin )) AS ?linkedinurl)
-                            BIND(CONCAT("https://", STR( ?domain )) AS ?domainurl)
-                            OPTIONAL { ?sub <http://globalcompany.org/yearFound> ?year .  }
-                    FILTER ( ?sub = """ + company_id + """ )} """
+                            OPTIONAL {?country <rdfs:label> ?country_label .}
+                            OPTIONAL {?industry <rdfs:label> ?industry_label .}
+                            OPTIONAL {?locality <rdfs:label> ?locality_label .}
+                            BIND(COALESCE(?domain, ?default_string) as ?result_domain)
+                            BIND(COALESCE(?industry_label, ?default_string) as ?result_industry)
+                            BIND(COALESCE(?size, ?default_string) as ?result_size)
+                            BIND(COALESCE(?locality_label, ?default_string) as ?result_locality)
+                            BIND(COALESCE(?country_label, ?default_string) as ?result_country)
+                            BIND(COALESCE(?linkedin, ?default_string) as ?result_linkedin)
+                            BIND(COALESCE(?current, ?default_string) as ?result_current)
+                            BIND(COALESCE(?total, ?default_string) as ?result_total)
+                            BIND(COALESCE(?year, ?default_string) as ?result_year)}
+                            LIMIT 1"""
 
     qres = g.query(stringquery)
     val = qres
--- companyinfo/views.py
+++ companyinfo/views.py
@@ -13,18 +13,57 @@ def search(request):
     return render(request, 'companyinfo/index.html', {'qres': qres})
 
 def info(request, rdf_object):
+    local_result = {}
+    online_result = {}
+    name = ''
+    web = ''
     qres = getCompanyData(rdf_object)
 
     for row in qres:
         name = row.str_name_label
+        local_result['name'] = row.str_name_label
+        local_result['country_label'] = row.str_country_label
+        local_result['industry_label'] = row.str_industry_label
+        local_result['year'] = row.str_year
+        local_result['size'] = row.str_size
+        local_result['locality_label'] = row.str_locality_label
+        local_result['current'] = row.str_current
+        local_result['total'] = row.str_total
+        local_result['linkedinurl'] = 'https://' + str(row.linkedinurl)
+        local_result['domainurl'] = 'https://' + str(row.domainurl)
+        web = str(row.domainurl)
 
-    qresonline = getCompanyDataOnline(name)
-    online_result = {}
-
+    qresonline = getCompanyDataOnline(name, web)
+    
     for result in qresonline["results"]["bindings"]:
-        pred = str(result["pred"]["value"]).split("/")[-1]
-        pred = pred.split("#")[-1]
-        obj = str(result["obj"]["value"]).split("/")[-1]
-        online_result[pred] = obj
+        online_result['topic'] = result["str_topic"]["value"]
+        online_result['wikipageid'] = result["str_wikipageid"]["value"]
+        online_result['latitude'] = result["str_latitude"]["value"]
+        online_result['longitude'] = result["str_longitude"]["value"]
+        online_result['abstract'] = result["str_abstract"]["value"]
+        online_result['assets'] = '${:,.2f}'.format(float(result["str_assets"]["value"]))
+        online_result['equity'] = '${:,.2f}'.format(float(result["str_equity"]["value"]))
+        online_result['location'] = result["str_location"]["value"]
+        online_result['netincome'] = '${:,.2f}'.format(float(result["str_netincome"]["value"]))
+        online_result['operatingincome'] = '${:,.2f}'.format(float(result["str_operatingincome"]["value"]))
+        online_result['revenue'] = '${:,.2f}'.format(float(result["str_revenue"]["value"]))
+        online_result['areaserved'] = result["str_areaserved"]["value"]
 
-    return render(request, 'companyinfo/company_details.html', {'qres': qres, 'online_result': online_result})
+    return render(request, 'companyinfo/company_details.html', {'local_result': local_result, 'online_result': online_result})
+
+    # def info(request, rdf_object):
+    # qres = getCompanyData(rdf_object)
+
+    # for row in qres:
+    #     name = row.str_name_label
+
+    # qresonline = getCompanyDataOnline(name)
+    # online_result = {}
+
+    # for result in qresonline["results"]["bindings"]:
+    #     # pred = str(result["pred"]["value"]).split("/")[-1]
+    #     # pred = pred.split("#")[-1]
+    #     # obj = str(result["obj"]["value"]).split("/")[-1]
+    #     # online_result[pred] = obj
+
+    # return render(request, 'companyinfo/company_details.html', {'qres': qres, 'online_result': online_result})
