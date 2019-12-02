0ddf1ce03b3ff81208dea8d4861ced5612459ed3
Syifa Nurhayati <syifanurhayati727@gmail.com>
2019-12-02 20:39:50 +0700

    fix

--- globalcompany/urls.py
+++ globalcompany/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('companyinfo/', include('companyinfo.urls')),
+    path('', include('companyinfo.urls')),
     path('admin/', admin.site.urls),
 ]
